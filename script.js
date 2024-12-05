44659bda4fe239c833c003d45a7f658d91cc3710
bilguun234 <73054712+bilguun234@users.noreply.github.com>
2024-12-05 17:58:01 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Example: Show an alert when a link is clicked
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", (event) => {
-        alert(`You clicked on ${event.target.textContent}`);
-    });
-});
